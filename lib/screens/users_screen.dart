167422a187f8546abb8ac67efa0862b9c8810d4a
Eduardo Perez <prz.rmz.eduardo@gmail.com>
2023-04-19 17:32:57 -0600

    user service change db

--- lib/screens/users_screen.dart
+++ lib/screens/users_screen.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:loan_app/models/user_model.dart';
+import 'package:loan_app/services/users_service.dart';
 import 'package:loan_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class UsersScreen extends StatelessWidget {
-
   static String routerName = 'Users';
-   
+
   const UsersScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('UsersScreen'),
+    final userService = Provider.of<UsersService>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Users"),
       ),
-       drawer: SideMenu(),
+      body: (userService.isLoading)
+          ? const Center(child: CircularProgressIndicator.adaptive())
+          : RefreshIndicator(
+              onRefresh: () => Future.delayed(Duration(seconds: 3)),
+              child: ListView.builder(
+                itemCount: userService.users.length,
+                itemBuilder: (context, index) {
+                  final user = userService.users[index];
+                  return _CardUser(user: user);
+                },
+              ),
+            ),
+      drawer: SideMenu(),
+    );
+  }
+}
+
+class _CardUser extends StatelessWidget {
+  final User user;
+  const _CardUser({super.key, required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 15),
+      color: Colors.white,
+      elevation: 0,
+      child: ListTile(
+        leading: Icon(Icons.person),
+        title: Text(user.name),
+        trailing: Icon(Icons.arrow_forward_ios),
+        subtitle: Text(user.email),
+      ),
+      
     );
   }
 }
--- lib/services/users_service.dart
+++ lib/services/users_service.dart
@@ -8,16 +8,30 @@ import 'package:loan_app/utils/dio_instance.dart';
 import '../models/models.dart';
 
 class UsersService extends ChangeNotifier {
-  
   final Dio _dio = DioInstance().dio;
 
+
+  // Containers
+  final List<User> users = [];
+
+  bool isLoading = true;
+  bool isError = false;
+  String error = '';
+
+  UsersService() {
+    loadUsers();
+  }
+  
   Future<List<User>> loadUsers() async {
-    
+    isLoading = true;
+    notifyListeners();
     final resp = await _dio.get('/users');
-    
+
     final respJson = Users.fromJson(resp.data);
-    // return respJson.users.map((e) => e.name);
-    print('me acrive');
+    users.addAll(respJson.users);
+    isLoading = false;
+    notifyListeners();
+
     return respJson.users;
   }
 }
